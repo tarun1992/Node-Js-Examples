6738aabec9ee13c5e0feffd9e7845fe5881971e7
tarun <tarunnarang1992@gmail.com>
2018-04-18 19:53:09 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter: (x, y) => (2*(x+y)),
+    area: (x, y) => x*y
+};
+
+function solveRect(l, b){
+    console.log("Rectangle l = "+l+" and b = "+b);
+    if(l <=0 || b <=0 ){
+        console.log("Length and breadth should be greater than zero.");
+    }
+    else{
+        console.log("The area of rectangle is "+rect.area(l, b));
+        console.log("The perimeter of rectangle is "+rect.perimeter(l, b));
+    }
+}
+
+solveRect(2,5);
+solveRect(0,3);
+solveRect(2,3);
+solveRect(3,0);
